c40f33f03e7dd965c42d3a767057f8624aba7e69
김승진 <ohksj77@gmail.com>
2024-02-10 13:15:49 +0900

    chore: init project

---
+++ src/test/java/com/kimseungjin/cafe/CafeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kimseungjin.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
