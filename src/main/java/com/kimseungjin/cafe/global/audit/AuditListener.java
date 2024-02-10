947b21154a54fe56ac3e9fd93e270846f4ec39ad
김승진 <ohksj77@gmail.com>
2024-02-10 18:15:03 +0900

    feat: add audit listener and softdelete repository

---
+++ src/main/java/com/kimseungjin/cafe/global/audit/AuditListener.java
@@ -0,0 +1,22 @@
+package com.kimseungjin.cafe.global.audit;
+
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.util.Optional;
+
+public class AuditListener {
+
+    @PrePersist
+    public void setCreatedAt(final Auditable auditable) {
+        final BaseTime baseTime =
+                Optional.ofNullable(auditable.getBaseTime()).orElseGet(BaseTime::new);
+        auditable.setBaseTime(baseTime);
+    }
+
+    @PreUpdate
+    public void setUpdatedAt(final Auditable auditable) {
+        final BaseTime baseTime = auditable.getBaseTime();
+        baseTime.update();
+    }
+}
