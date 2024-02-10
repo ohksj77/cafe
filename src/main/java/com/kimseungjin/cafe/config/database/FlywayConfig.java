54665a09dfc2f72104578d4fa6e83c72a2167727
김승진 <ohksj77@gmail.com>
2024-02-10 14:55:37 +0900

    fix: resolve flyway error with mysql:5.7

---
+++ src/main/java/com/kimseungjin/cafe/config/database/FlywayConfig.java
@@ -0,0 +1,24 @@
+package com.kimseungjin.cafe.config.database;
+
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class FlywayConfig {
+
+    private static final String LOCATION = "classpath:db/migration";
+    private final DataSource dataSource;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations(LOCATION)
+                .load();
+    }
+}
